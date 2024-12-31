dd17ba4b1121298aef12d0ab0d3394a16727a761
Jeison Cabarcas <jeisoncabarcaspertuz@gmail.com>
2024-12-31 16:10:42 -0500

    se crea configuracion de Dockerfile

--- src/components/Tables/RecordsTable.tsx
+++ src/components/Tables/RecordsTable.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './RecordsTable.module.css';
+import React from "react";
+import styles from "./RecordsTable.module.css";
 
 interface Record {
   id: number;
@@ -7,6 +7,7 @@ interface Record {
   lastName: string;
   modelPc: string;
   email: string;
+  createdDate: string;
 }
 
 interface RecordsTableProps {
@@ -17,30 +18,34 @@ interface RecordsTableProps {
 const RecordsTable: React.FC<RecordsTableProps> = ({ records, onBack }) => {
   return (
     <div className={styles.tableContainer}>
-      <h2>Registros</h2>
-      <table className={styles.recordsTable}>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Modelo PC</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {records.map((record) => (
-            <tr key={record.id}>
-              <td>{record.id}</td>
-              <td>{record.firstName}</td>
-              <td>{record.lastName}</td>
-              <td>{record.modelPc}</td>
-              <td>{record.email}</td>
+      <h2 className={styles.tableTitle}>Registros</h2>
+      <div className={styles.tableWrapper}>
+        <table className={styles.recordsTable}>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Modelo PC</th>
+              <th>Email</th>
+              <th>Fecha y hora</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <button onClick={onBack} className={styles.backButton}>Volver al formulario</button>
+          </thead>
+          <tbody>
+            {records.map((record) => (
+              <tr key={record.id}>
+                <td data-label="Nombre">{record.firstName}</td>
+                <td data-label="Apellido">{record.lastName}</td>
+                <td data-label="Modelo PC">{record.modelPc}</td>
+                <td data-label="Email">{record.email}</td>
+                <td data-label="Fecha y hora">{record.createdDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <button onClick={onBack} className={styles.backButton}>
+        Volver al formulario
+      </button>
     </div>
   );
 };
